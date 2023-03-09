c00e365849f250a48c3381d554703574c91d2c1c
leafarpontes <rafapontes2008@hotmail.com>
2023-03-09 16:48:52 -0300

    added accessibility - pressing keyboard keys also work as a click

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -5,6 +5,10 @@ buttons.forEach(button => {
   const className = button.classList[1];
   sounds.forEach(sound => {
     const id = sound.id.substring(sound.id.indexOf('_') + 1);
-    className === id ? button.addEventListener('click', () => sound.play()) : null
+    if (className === id) {
+      button.addEventListener('click', (e) => sound.play());
+      button.addEventListener('keydown', (e) => e.code === "Space" || e.code === "Enter" ? button.classList.add('ativa') : null);
+      button.addEventListener('keyup', (e) => button.classList.remove('ativa'));
+    }
   })
 })
