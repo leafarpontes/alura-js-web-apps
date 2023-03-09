3736a5e1f7ba13d0eb38b3725df1e37c161ca183
leafarpontes <rafapontes2008@hotmail.com>
2023-03-08 22:06:34 -0300

    less code

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,17 +1,13 @@
 const buttons = document.querySelectorAll('.tecla');
 const sounds = document.querySelectorAll("[id^='som_']");
 
-const playSound = (sound) => {
-  sound.play();
-}
-
 buttons.forEach(button => {
   const className = button.classList[1];
   sounds.forEach(sound => {
     let [first, ...id] = sound.id.split('_');
     id = id.join('_');
     if (className === id) {
-      button.addEventListener('click', () => playSound(sound));
+      button.addEventListener('click', () => sound.play());
     }
   })
 })
