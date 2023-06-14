db3c6011270d8d796b1199186e7651c7134b0226
Rafael Pontes <rpontes@mundolivre.digital>
2023-06-14 16:07:13 -0300

    mochila-de-viagem: added update localstore item feature

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -6,27 +6,45 @@ form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const name = evt.target.elements['nome'];
   const quantity = evt.target.elements['quantidade'];
-  const itemToAdd = createElement(name.value, quantity.value);
+  const exists = items.find(el => el.name === name.value);
+  const itemToAdd = {
+    "name": name.value,
+    "quantity": quantity.value
+  }
+
+  if (exists) {
+    itemToAdd.id = exists.id;
+    updateElement(itemToAdd);
+    items[exists.id] = itemToAdd;
+  } else {
+    itemToAdd.id = items.length;
+    createElement(itemToAdd);
+    items.push(itemToAdd);
+  }
+  
   name.value = "";
   quantity.value = "";
-  items.push(itemToAdd);
+  
   localStorage.setItem("items", JSON.stringify(items));
 })
 
-const createElement = (name, quantity) => {
+const createElement = (itemToAdd) => {
   const newItem = document.createElement("li");
   newItem.classList.add("item");
   const itemNumber = document.createElement("strong");
-  itemNumber.innerHTML = quantity;
+  itemNumber.innerHTML = itemToAdd.quantity;
+  itemNumber.dataset.id = itemToAdd.id;
   newItem.appendChild(itemNumber);
-  newItem.innerHTML += name;
+  newItem.innerHTML += itemToAdd.name;
   list.appendChild(newItem);
+}
 
-  return {name, quantity};
+const updateElement = (itemToAdd) => {
+  document.querySelector(`[data-id="${itemToAdd.id}"]`).innerHTML = itemToAdd.quantity;
 }
 
 if (items.length > 0) {
   items.forEach(item => {
-    createElement(item.name, item.quantity);
+    createElement({name: item.name, quantity: item.quantity, id: item.id});
   })
 }
